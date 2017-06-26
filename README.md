e17da0a9dd0e1010b188392f7c6d0425ae178360
malo <malo@Malos-MacBook-Pro.local>
2017-06-26 16:48:39 -0700

    Clean folder and add license

--- README.md
+++ README.md
@@ -1,18 +1,38 @@
-# Demo of a self-driving car teering model for Tensorport
-
-## 
+# Demo of a self-driving car steering model for Tensorport
 
 
-## the comma.ai driving dataset
+
 
 ## Dataset
+The dataset consists of 10 videos clips of variable size recorded at 20 Hz
+with a camera mounted on the windshield of an Acura ILX 2016. In parallel to the videos
+we also recorded some measurements such as car's speed, acceleration,
+steering angle, GPS coordinates, gyroscope angles. See the full `log` list [here](Logs.md).
+These measurements are transformed into a uniform 100 Hz time base.
+
+The dataset folder structure is the following:
+```bash
++-- dataset
+|   +-- camera
+|   |   +-- 2016-04-21--14-48-08
+|   |   ...
+|   +-- log
+|   |   +-- 2016-04-21--14-48-08
+|   |   ...
+```
+
+All the files come in hdf5 format and are named with the time they were recorded.
+The camera dataset has shape `number_frames x 3 x 160 x 320` and `uint8` type.
+One of the `log` hdf5-datasets is called `cam1_ptr` and addresses the alignment
+between camera frames and the other measurements.
+
 
 ## Requirements
-[anaconda](https://www.continuum.io/downloads)  
-[tensorflow-0.9](https://github.com/tensorflow/tensorflow)  
-[keras-1.0.6](https://github.com/fchollet/keras)  
-[cv2](https://anaconda.org/menpo/opencv3)
+
+[tensorport-0.8.9]
+[tensorflow-1.0](https://github.com/tensorflow/tensorflow)  
 
 
 ## Credits
-https://github.com/commaai/research
+Author: Malo Marrec, malo@goodailab.com, (c) GoodAILab as specified in LICENSE
+Data and data_reader.py -  https://github.com/commaai/research - Licensed as specified in LICENSE COMMA
--- main_tf.py
+++ main_tf.py
@@ -1,3 +1,8 @@
+# MIT License, see LICENSE
+# Copyright (c) 2017 TensorPort
+# Author: Malo Marrec, malo@goodailab.com
+
+
 import numpy as np
 import h5py
 import time
@@ -10,226 +15,12 @@ import json
 #tport 
 from tensorport import TensorportClient as tport
 
+from models.model import *
+from utils.data_reader import *
+
 #Create logging
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-
-# From comma.ai
-
-def concatenate(camera_names, time_len):
-	logs_names = [x.replace('camera', 'log') for x in camera_names]
-
-	angle = []  # steering angle of the car
-	speed = []  # steering angle of the car
-	hdf5_camera = []  # the camera hdf5 files need to continue open
-	c5x = []
-	filters = []
-	lastidx = 0
-
-	for cword, tword in zip(camera_names, logs_names):
-		try:
-			with h5py.File(tword, "r") as t5:
-				c5 = h5py.File(cword, "r")
-				hdf5_camera.append(c5)
-				x = c5["X"]
-				c5x.append((lastidx, lastidx+x.shape[0], x))
-
-				speed_value = t5["speed"][:]
-				steering_angle = t5["steering_angle"][:]
-				idxs = np.linspace(0, steering_angle.shape[0]-1, x.shape[0]).astype("int")  # approximate alignment
-				angle.append(steering_angle[idxs])
-				speed.append(speed_value[idxs])
-
-				goods = np.abs(angle[-1]) <= 200
-
-				filters.append(np.argwhere(goods)[time_len-1:] + (lastidx+time_len-1))
-				lastidx += goods.shape[0]
-				# check for mismatched length bug
-				print("x {} | t {} | f {}".format(x.shape[0], steering_angle.shape[0], goods.shape[0]))
-				if x.shape[0] != angle[-1].shape[0] or x.shape[0] != goods.shape[0]:
-					raise Exception("bad shape")
-
-		except IOError:
-			import traceback
-			traceback.print_exc()
-			print "failed to open", tword
-
-	angle = np.concatenate(angle, axis=0)
-	speed = np.concatenate(speed, axis=0)
-	filters = np.concatenate(filters, axis=0).ravel()
-	print "training on %d/%d examples" % (filters.shape[0], angle.shape[0])
-	return c5x, angle, speed, filters, hdf5_camera
-
-first = True
-
-def datagen(data_dir, time_len=1, batch_size=256, ignore_goods=False):
-	"""
-	Parameters:
-	-----------
-	leads : bool, should we use all x, y and speed radar leads? default is false, uses only x
-	
-	data_dir: path to the data directory
-	"""
-	global first
-	assert time_len > 0
-
-
-	filter_files = os.listdir(data_dir)
-	filter_files = [data_dir + "/" + f for f in filter_files if f.endswith(".h5")] #sorint .h5 + full path
-	
-	filter_names = sorted(filter_files)
-
-	logger.info("Loading {} hdf5 buckets.".format(len(filter_names)))
-
-	c5x, angle, speed, filters, hdf5_camera = concatenate(filter_names, time_len=time_len)
-	filters_set = set(filters)
-
-	logger.info("camera files {}".format(len(c5x)))
-
-	X_batch = np.zeros((batch_size, time_len, 3, 160, 320), dtype='uint8')
-	angle_batch = np.zeros((batch_size, time_len, 1), dtype='float32')
-	speed_batch = np.zeros((batch_size, time_len, 1), dtype='float32')
-
-	while True:
-		try:
-			t = time.time()
-
-			count = 0
-			start = time.time()
-			while count < batch_size:
-				if not ignore_goods:
-					i = np.random.choice(filters)
-					# check the time history for goods
-					good = True
-					for j in (i-time_len+1, i+1):
-						if j not in filters_set:
-							good = False
-					if not good:
-						continue
-
-				else:
-					i = np.random.randint(time_len+1, len(angle), 1)
-
-				# GET X_BATCH
-				# low quality loop
-				for es, ee, x in c5x:
-					if i >= es and i < ee:
-						X_batch[count] = x[i-es-time_len+1:i-es+1]
-						break
-
-				angle_batch[count] = np.copy(angle[i-time_len+1:i+1])[:, None]
-				speed_batch[count] = np.copy(speed[i-time_len+1:i+1])[:, None]
-
-				count += 1
-
-			# sanity check
-			assert X_batch.shape == (batch_size, time_len, 3, 160, 320)
-
-			# logging.debug("loading image took: {}s".format(time.time()-t))
-			# print("%5.2f ms" % ((time.time()-start)*1000.0))
-
-			if first:
-				print "X", X_batch.shape
-				print "angle", angle_batch.shape
-				print "speed", speed_batch.shape
-				first = False
-
-			yield (X_batch, angle_batch, speed_batch)
-
-		except KeyboardInterrupt:
-			raise
-		except:
-			traceback.print_exc()
-			pass
-
-
-def gen(data_dir, time_len=1, batch_size=256, ignore_goods=False):
-	"""" Wrapper for datagen, taking only steering angle"""
-	for data_row in datagen(data_dir, time_len, batch_size, ignore_goods):
-		X, Y, _ = data_row
-		Y = Y[:, -1]
-		if X.shape[1] == 1:  # no temporal context
-			X = X[:, -1]
-		yield X, Y
-
-#My code
-
-def get_model(X):
-	#ch, row, col = 3, 160, 320
-	#Add the lambda here. Is it a mean substraction?
-
-	conv1 = tf.layers.conv2d(
-		tf.reshape(X,[FLAGS.batch,3,160,320]),
-		filters = 16,
-		kernel_size = (8,8),
-		strides=(4, 4),
-		padding='same',
-		kernel_initializer=tf.contrib.layers.xavier_initializer(),
-		bias_initializer=tf.zeros_initializer(),
-		kernel_regularizer=None,
-		name = 'conv1',
-		activation = tf.nn.elu
-		)
-
-	conv2 = tf.layers.conv2d(
-		conv1 ,
-		filters = 32,
-		kernel_size = (5,5),
-		strides=(2, 2),
-		padding='same',
-		kernel_initializer=tf.contrib.layers.xavier_initializer(),
-		bias_initializer=tf.zeros_initializer(),
-		kernel_regularizer=None,
-		name = 'conv2',
-		activation = tf.nn.elu
-		)
-
-	conv3 = tf.layers.conv2d(
-		conv2,
-		filters = 64,
-		kernel_size = (5,5),
-		strides=(2, 2),
-		padding='same',
-		kernel_initializer=tf.contrib.layers.xavier_initializer(),
-		bias_initializer=tf.zeros_initializer(),
-		kernel_regularizer=None,
-		name = 'conv3',
-		activation = None
-		)
-
-	f1 = tf.contrib.layers.flatten(conv3)
-	d1 = tf.nn.dropout(f1, FLAGS.dropout_rate1)
-	e1 = tf.nn.elu(d1)
-	dense1 = tf.layers.dense(e1, units = FLAGS.fc_dim)
-	d2 = tf.nn.dropout(dense1, FLAGS.dropout_rate2)
-	e2 = tf.nn.elu(d2)	
-	dense2 = tf.layers.dense(e2, units = 1)
-
-	return dense2
-
-
-def get_loss(predictions,labels):
-	# loss = tf.nn.l2_loss(predictions-labels)
-	loss = tf.reduce_mean(tf.square(predictions-labels))
-	return loss
-
-
-
-def variable_summaries(var):
- 	"""Attach a lot of summaries to a Tensor (for TensorBoard visualization).
-	credits: https://www.tensorflow.org/get_started/summaries_and_tensorboard
- 	"""
- 	with tf.name_scope('summaries'):
-		mean = tf.reduce_mean(var)
-		tf.summary.scalar('mean', mean)
-		with tf.name_scope('stddev'):
-			stddev = tf.sqrt(tf.reduce_mean(tf.square(var - mean)))
-		tf.summary.scalar('stddev', stddev)
-		tf.summary.scalar('max', tf.reduce_max(var))
-		tf.summary.scalar('min', tf.reduce_min(var))
-		tf.summary.histogram('histogram', var)
-
 
 if __name__ == "__main__":
 
@@ -420,5 +211,7 @@ if __name__ == "__main__":
 				current_loss, v_summary = sess.run(variables, feed_dict)
 				summary_writer.add_summary(v_summary,i)
 				print("... Epoch %s - Validation loss: %s" % (e,current_loss))
+				i = 1 # resetting iteration counters
+				e += 1 # updating epoch number
 
 
