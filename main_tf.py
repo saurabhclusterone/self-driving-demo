e5a46bb162f970b51212692b1c9638ee33b0a5d2
malo <malo@Malos-MacBook-Pro.local>
2017-06-27 14:03:19 -0700

    Removing hacky lines

--- main_tf.py
+++ main_tf.py
@@ -50,25 +50,25 @@ if __name__ == "__main__":
 	# tport snippet 2: flags. 
 
 	#Define the path from the root data directory to your data.
-	# Here the data directory are split into training and validation, so defining two flags
-	# flags.DEFINE_string(
-	# 	"train_data_dir",
-	# 	tport().get_data_path(root=ROOT_PATH_TO_LOCAL_DATA,path='/camera/training'),
-	# 	"""Path to training dataset. It is recommended to use get_data_path() 
-	# 	to define your data directory. If you set your dataset directory manually make sure to use /data/ 
-	# 	as root path when running on TensorPort cloud."""
-	# 	)
-	# flags.DEFINE_string(
-	# 	"val_data_dir",
-	# 	tport().get_data_path(root=ROOT_PATH_TO_LOCAL_DATA,path='/camera/validation'),
-	# 	"Path to validation dataset."
-	# 	)
-	# flags.DEFINE_string("logs_dir",
-	# 	tport().get_logs_path(root=PATH_TO_LOCAL_LOGS),
-	# 	"Path to store logs and checkpoints. It is recommended"
-	# 	"to use get_logs_path() to define your logs directory."
-	# 	"If you set your logs directory manually make sure"
-	# 	"to use /logs/ when running on TensorPort cloud.")
+	#Here the data directory are split into training and validation, so defining two flags
+	flags.DEFINE_string(
+		"train_data_dir",
+		tport().get_data_path(root=ROOT_PATH_TO_LOCAL_DATA,path='/camera/training'),
+		"""Path to training dataset. It is recommended to use get_data_path() 
+		to define your data directory. If you set your dataset directory manually make sure to use /data/ 
+		as root path when running on TensorPort cloud."""
+		)
+	flags.DEFINE_string(
+		"val_data_dir",
+		tport().get_data_path(root=ROOT_PATH_TO_LOCAL_DATA,path='/camera/validation'),
+		"Path to validation dataset."
+		)
+	flags.DEFINE_string("logs_dir",
+		tport().get_logs_path(root=PATH_TO_LOCAL_LOGS),
+		"Path to store logs and checkpoints. It is recommended"
+		"to use get_logs_path() to define your logs directory."
+		"If you set your logs directory manually make sure"
+		"to use /logs/ when running on TensorPort cloud.")
 	flags.DEFINE_string("job_name", job_name,
 						"job name: worker or ps")
 	flags.DEFINE_integer("task_index", task_index,
@@ -83,19 +83,19 @@ if __name__ == "__main__":
 	# end of tport snippet 2
 
 
-	#DEBUG
-	flags.DEFINE_string("train_data_dir","/Users/malo/Documents/comma/comma-data/camera/training","")
-	flags.DEFINE_string("val_data_dir","/Users/malo/Documents/comma/comma-data/camera/validation","")
-	flags.DEFINE_string("logs_dir","/Users/malo/Documents/tensorport-self-driving-demo/logs","")
+	# #DEBUG
+	# flags.DEFINE_string("train_data_dir","/Users/malo/Documents/comma/comma-data/camera/training","")
+	# flags.DEFINE_string("val_data_dir","/Users/malo/Documents/comma/comma-data/camera/validation","")
+	# flags.DEFINE_string("logs_dir","/Users/malo/Documents/tensorport-self-driving-demo/logs","")
 
 
 
 	# Training flags
 	flags.DEFINE_integer("batch",256,"Batch size")
 	flags.DEFINE_integer("time",1,"Number of frames per sample")
-	flags.DEFINE_integer("steps_per_epoch",10000,"Number of training steps per epoch")
+	flags.DEFINE_integer("steps_per_epoch",1000,"Number of training steps per epoch")
 	flags.DEFINE_integer("nb_val_steps",1,"Number of training steps")
-	flags.DEFINE_integer("nb_epochs",10,"Number of epochs")
+	flags.DEFINE_integer("nb_epochs",20,"Number of epochs")
 
 
 	# Model flags
@@ -109,10 +109,9 @@ if __name__ == "__main__":
 	flags.DEFINE_integer("fc_dim",512,"Size of the dense layer")
 	flags.DEFINE_boolean("nogood",False,"Ignore `goods` filters.")
 
-	print(ROOT_PATH_TO_LOCAL_DATA)
+
 	print(FLAGS.train_data_dir)
-	print()
-	print(tport().get_data_path(root=ROOT_PATH_TO_LOCAL_DATA,path='/camera/training'))
+
 	# tport snippet 3: configure distributed
 	def device_and_target():
 		# If FLAGS.job_name is not set, we're running single-machine TensorFlow.
@@ -224,7 +223,7 @@ if __name__ == "__main__":
 				current_loss, t_summary, lr, _ = sess.run(variables, feed_dict)
 				summary_writer.add_summary(t_summary,i)
 				# print("Epoch %s, iteration %s - Learning Rate: %f, Batch loss: %s" % (e,i,lr,current_loss))
-				print("Iteration %s - Batch loss: %s" % (epoch_index*FLAGS.steps_per_epoch + i,current_loss))
+				print("Iteration %s - Batch loss: %s" % ((epoch_index-1)*FLAGS.steps_per_epoch + i,current_loss))
 				i+=1
 
 	#Left to the reader - that could be done in a continuous fashion on a separate worker / thread
